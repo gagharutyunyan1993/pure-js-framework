d779d234529b6527ce6739a389c707d71405d94b
Gagik Harutyunyan <gagharutyunyan1993@gmail.com>
2021-11-06 19:13:37 +0400

    add listener for events

--- src/app/pages/home-page.component.js
+++ src/app/pages/home-page.component.js
@@ -1,8 +1,27 @@
-import {WFMComponent} from "../../main";
+import {router, WFMComponent} from "../../main";
 
-class HomePageComponent extends WFMComponent{
+class HomePageComponent extends WFMComponent {
     constructor(config) {
         super(config);
+    }
+
+    events() {
+        return {
+            'click .js-link': 'goToTabs'
+        }
+    }
+
+    onInit() {
+        console.log('component init');
+    }
+
+    afterInit() {
+        console.log('component after init');
+    }
+
+    goToTabs(event) {
+        event.preventDefault();
+        router.navigate('tabs')
     }
 }
 
@@ -17,7 +36,7 @@ export const homePageComponent = new HomePageComponent({
                   <p>No functional</p>
                 </div>
                 <div class="card-action">
-                  <a href="#">Go to another page</a>
+                  <a href="#not-existing-path" class="js-link">Go to another page</a>
                 </div>
               </div>
             </div>
--- src/main/core/module.js
+++ src/main/core/module.js
@@ -39,6 +39,8 @@ export class Module {
     }
 
     renderComponent(c) {
+        if(!wfm.isUndefined(c.onInit)) c.onInit();
         c.render();
+        if(!wfm.isUndefined(c.afterInit)) c.afterInit();
     }
 }
